21ebbf756d779a7c73b756070b3440ecabf5de12
gorsestarrr <arpodymov@gmail.com>
2024-12-07 23:25:00 +0100

    2024 day 7 refactoring

--- 2024/day_07/main.py
+++ 2024/day_07/main.py
@@ -1,46 +1,38 @@
 import itertools
 
 
-def generate_combinations(n, array):
-    return [list(combination) for combination in itertools.product(array, repeat=n)]
+def generate_operators(n, operators_set):
+    return [list(combination) for combination in itertools.product(operators_set, repeat=n)]
+
+
+def execute_operator(operator, x, y):
+    if operator == 0:
+        return x + y
+    if operator == 1:
+        return x * y
+    return int(str(x) + str(y))
+
+
+def solve(equations, operators_set):
+    result_sum = 0
+    for equation in equations:
+        target, numbers = equation
+        for operator in generate_operators(len(numbers), operators_set):
+            execution = numbers[0]
+            for index, number in enumerate(numbers[1:], start=1):
+                execution = execute_operator(operator[index - 1], execution, number)
+            if execution == target:
+                result_sum += execution
+                break
+    return result_sum
 
 
 def part1(equations):
-    sum = 0
-    for equation in equations:
-        target, numbers = equation
-        operators = generate_combinations(len(numbers), [0, 1])  # 0 -> +, 1 -> *
-        for operator in operators:
-            execution = numbers[0]
-            for index, number in enumerate(numbers[1:], start=1):
-                if operator[index - 1] == 0:
-                    execution += number
-                else:
-                    execution *= number
-            if execution == target:
-                sum += execution
-                break
-    return sum
+    return solve(equations, [0, 1])
+
 
 def part2(equations):
-    sum = 0
-    for equation in equations:
-        target, numbers = equation
-        operators = generate_combinations(len(numbers), [0, 1, 2])  # 0 -> +, 1 -> *, 2 -> ||
-        for operator in operators:
-            execution = numbers[0]
-            for index, number in enumerate(numbers[1:], start=1):
-                if operator[index - 1] == 0:
-                    execution += number
-                elif operator[index - 1] == 1:
-                    execution *= number
-                else:
-                    execution = int(str(execution) + str(number))
-
-            if execution == target:
-                sum += execution
-                break
-    return sum
+    return solve(equations, [0, 1, 2])
 
 
 if __name__ == '__main__':
